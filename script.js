819bcb8b078de19e4cdc4b83d2431f60b9d74013
vinayaksoni1729 <113187290+vinayaksoni1729@users.noreply.github.com>
2023-06-07 10:14:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var names=["Yaakov","John","Jim","Mike","Tom","Paul"]; 
+
+for (var i=0;i<names.length; i++){
+    if(names[i].charAt(0)=="j"||names[i].charAt(0)=="J"){
+        console.log("Goodbye "+names[i])
+    }
+    else{
+        console.log("Hello "+names[i])
+    }
+}
